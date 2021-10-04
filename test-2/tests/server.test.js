4fc7c056ecab70d30035d5da840aab6de0ed8fcd
Irfan Marzuki <irfnmzk@gmail.com>
2021-10-04 11:12:12 +0000

    chore: update gitpod.yml

--- test-2/tests/server.test.js
+++ test-2/tests/server.test.js
@@ -1,7 +1,12 @@
 const request = require("supertest");
 const { initializeApp } = require("../src");
+const { database } = require("../src/config/database");
 
 describe("API ENDPOINT", () => {
+  beforeAll(async () => {
+    await database.sync();
+  });
+
   it("should return correct movie with id tt0093175", async () => {
     const app = await initializeApp();
     const result = await request(app).get("/movie/tt0093175");
