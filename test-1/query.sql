0ebb66b644377868df507b8c91627a110169cd0b
Irfan Marzuki <irfnmzk@gmail.com>
2021-10-04 06:36:20 +0000

    feat: finish query parent name

--- test-1/query.sql
+++ test-1/query.sql
@@ -1,5 +1,5 @@
 ------ CREATE USER TABLE ------
-CREATE TABLE tb_user(
+CREATE TABLE user_tbl(
   id INTEGER not null,
   username varchar(30) not null,
   parent_id integer not null,
@@ -7,9 +7,21 @@ CREATE TABLE tb_user(
 );
 
 ----- INSERT VALUE TO USER TABLE -----
-INSERT INTO user (id,username ,parent_id) VALUES
+INSERT INTO user_tbl (id,username ,parent_id) VALUES
   (1, 'Ali',2),
   (2, 'Budi',0),
   (3, 'Cecep',1);
 
 ----- Query to find parent name -----
+SELECT
+    user.id,
+    user.username,
+    parent.username as parent
+from
+    user_tbl as user
+left join
+    user_tbl as parent
+on
+    user.parent_id = parent.id
+order by
+    user.id asc
